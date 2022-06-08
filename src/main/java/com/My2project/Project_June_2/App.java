fde78b0355cc26ed147943ee6e0023d219d61538
Fahmida <fahmida49@gmail.com>
2022-06-08 15:09:53 -0400

    first commit

---
+++ src/main/java/com/My2project/Project_June_2/App.java
@@ -0,0 +1,13 @@
+package com.My2project.Project_June_2;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/test/java/June_Package/June_Class.java
@@ -0,0 +1,10 @@
+package June_Package;
+
+public class June_Class {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
